9b1172ac482a6f8c70f7ae9efbf0480f3d08404a
Philipp Bredol <philipp.bredol@rwth-aachen.de>
2025-02-18 22:42:45 +0100

    tweak build script

--- dev/build.sh
+++ dev/build.sh
@@ -8,15 +8,6 @@ if [[ -e "freecad" ]]; then
   exit 1
 fi
 
-# sync with github if upload is enabled
-if [[ "$1" == "--upload" ]]; then
-  git pull \
-  && git pull --tags \
-  && git push \
-  && git push --tags \
-  || exit 1
-fi
-
 # This removes the freecad-link before building and uploading to PyPi, to prevent building as a namespace
 # package. It has to be done this way, because using include/exclude options in pyproject.toml did not make
 # hatch ingore the freecad symlink (not sure if this is a bug or intentional).
@@ -36,4 +27,11 @@ if [[ "$1" == "--upload" ]]; then
 
   # restore link and remove src/optics_design_workbench copy
   ln -s src freecad
+
+  # make new commit with restored freecad link and shift latest version tag 
+  ver="$(./dev/update-packagexml.py)"
+  git tag -d $ver
+  git add freecad
+  git commit -m 'restore freecad link'
+  git tag $ver
 fi
