c5e958e5a0a719530d2abe8a091eb67d89b48a59
Philipp Bredol <philipp.bredol@rwth-aachen.de>
2025-02-18 22:34:28 +0100

    increment version number

--- dev/build.sh
+++ dev/build.sh
@@ -3,6 +3,11 @@
 # change dir to this script's location
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")/.."
 
+if [[ -e "freecad" ]]; then
+  echo "freecad link exists, please call commit-and-tag" script before building release
+  exit 1
+fi
+
 # sync with github if upload is enabled
 if [[ "$1" == "--upload" ]]; then
   git pull \
@@ -22,14 +27,13 @@ fi
 # For FreeCADs add-on manager, which downloads from the github repo, this project will look like a namespace
 # package, because of the freecad -> src symlink.
 # Hopefully the freecad base package will be added to PyPi in the future, to remove necessity for this mess.
-rm freecad \
-&& rm -rf dist \
-&& uv build \
-
-# restore link and remove src/optics_design_workbench copy
-ln -s src freecad
+rm -rf dist \
+&& hatch build \
 
 # upload if enabled
 if [[ "$1" == "--upload" ]]; then
   uvx uv-publish
+
+  # restore link and remove src/optics_design_workbench copy
+  ln -s src freecad
 fi
