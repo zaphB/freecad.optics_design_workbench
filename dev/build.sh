69b4d2ef4df9b2e2d16ffcf5d06fb10822405b1d
Philipp Bredol <philipp.bredol@rwth-aachen.de>
2025-02-22 20:45:28 +0100

    remove freecad namespace entirely from PyPi builds, combining namespaced and non-namespaced variants in one package is hopeless

--- dev/build.sh
+++ dev/build.sh
@@ -5,10 +5,8 @@ cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")/.."
 
 # clear dist folder and build freshly, create full copy of optics_design_workbench folder
 # to make it available both via 'import freecad.optics_design_workbench' and
-# 'import optics_design_workbench'. After building restore symlink.
-rm -rf dist \
-&& rm -rf optics_design_workbench \
-&& cp -r freecad/optics_design_workbench . \
+# 'import optics_design'. After building restore symlink.
+rm -rf dist/* \
 && hatch build
 
 # upload if enabled
