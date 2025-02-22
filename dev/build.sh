d1a52d6cf6392b809f720fb4046296576d72bbe7
Philipp Bredol <philipp.bredol@rwth-aachen.de>
2025-02-22 20:26:44 +0100

    fix empty wheel issue again...

--- dev/build.sh
+++ dev/build.sh
@@ -3,9 +3,13 @@
 # change dir to this script's location
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")/.."
 
-# clear dist folder and build freshly
+# clear dist folder and build freshly, create full copy of optics_design_workbench folder
+# to make it available both via 'import freecad.optics_design_workbench' and
+# 'import optics_design_workbench'. After building restore symlink.
 rm -rf dist \
-&& uv build
+&& rm -rf optics_design_workbench \
+&& cp -r freecad/optics_design_workbench . \
+&& hatch build
 
 # upload if enabled
 if [[ "$1" == "--upload" ]]; then
