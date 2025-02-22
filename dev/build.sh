df82258eb421c4b9b16835b91aa855922adb2fed
Philipp Bredol <philipp.bredol@rwth-aachen.de>
2025-02-22 20:00:36 +0100

    rename src -> freecad, otherwise addon manager will not be able to install workbench

--- dev/build.sh
+++ dev/build.sh
@@ -3,35 +3,11 @@
 # change dir to this script's location
 cd -P -- "$(dirname -- "${BASH_SOURCE[0]}")/.."
 
-if [[ -e "freecad" ]]; then
-  echo "freecad link exists, please call commit-and-tag" script before building release
-  exit 1
-fi
-
-# This removes the freecad-link before building and uploading to PyPi, to prevent building as a namespace
-# package. It has to be done this way, because using include/exclude options in pyproject.toml did not make
-# hatch ingore the freecad symlink (not sure if this is a bug or intentional).
-# The package uploaded to PyPi has to be expose a non-namespace 'optics_design_workbench' package, because
-# the freecad namespace base package is not pip-installable (e.g. to virtual environments). Running 
-# "import freecad.optics_design_workbench" will therefore fail with "module freecad not found" in virtual 
-# environments.
-# For FreeCADs add-on manager, which downloads from the github repo, this project will look like a namespace
-# package, because of the freecad -> src symlink.
-# Hopefully the freecad base package will be added to PyPi in the future, to remove necessity for this mess.
+# clear dist folder and build freshly
 rm -rf dist \
-&& hatch build \
+&& uv build
 
 # upload if enabled
 if [[ "$1" == "--upload" ]]; then
   uvx uv-publish
-
-  # restore link and remove src/optics_design_workbench copy
-  ln -s src freecad
-
-  # make new commit with restored freecad link and shift latest version tag 
-  ver="$(./dev/update-packagexml.py)"
-  git tag -d "v$ver"
-  git add freecad
-  git commit -m 'restore freecad link'
-  git tag "v$ver"
 fi
