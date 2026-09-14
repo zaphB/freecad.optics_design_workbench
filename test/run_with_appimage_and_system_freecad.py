1df9b7c0b74dc1dade12842d3a5b8955e1b065d3
Philipp Bredol <philipp.bredol@rwth-aachen.de>
2026-09-14 20:32:12 +0200

    add workaround to make FreeCAD AppImages work

--- test/run_with_appimage_and_system_freecad.py
+++ test/run_with_appimage_and_system_freecad.py
@@ -9,12 +9,18 @@ def pytest_generate_tests(metafunc):
     latestAppImage = '../'+latestAppImage
   else:
     raise ValueError(f'failed to find latest-freecad.AppImage, did you run ./dev/download-latest-appimage.sh? ({os.path.abspath('.')=})')
-  metafunc.parametrize("envValue", ["", os.path.abspath(latestAppImage)], indirect=True)
+  
+  # LD_PRELOAD workaround: LD_PRELOAD prefix is necessary on my dev-machine to launch appimage, otherwise it will die with GLX loading error
+  metafunc.parametrize('envValue', ['', f'LD_PRELOAD=/usr/lib/libdrm_amdgpu.so.1 {os.path.abspath(latestAppImage)}'], indirect=True)
 
 @pytest.fixture(autouse=True)
 def envValue(request, monkeypatch):
   param = getattr(request, "param", None)
-  if param is not None:
-    print(f'setting TEST_FREECAD_BINARY to {repr(param)}')
-    monkeypatch.setenv('TEST_FREECAD_BINARY', param)
+  if param is not None and param.strip():
+    _s = param.split()
+    bina = _s.pop(-1)
+    envs = ' '.join(_s)
+    print(f'setting TEST_FREECAD_BINARY to {repr(bina)}, with env vars: {repr(envs)}')
+    monkeypatch.setenv('TEST_FREECAD_BINARY', bina)
+    monkeypatch.setenv('TEST_FREECAD_ENVS', envs)
   yield param
